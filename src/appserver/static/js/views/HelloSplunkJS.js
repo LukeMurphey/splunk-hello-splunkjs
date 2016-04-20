3cf26979ff9a98dc3209494c24e22f8e88730919
Luke Murphey <Luke@LukeMurphey.net>
2016-04-20 13:56:16 -0500

    Adding template of Javascript

---
+++ src/appserver/static/js/views/HelloSplunkJS.js
@@ -0,0 +1,41 @@
+define([
+    "underscore",
+    "backbone",
+    "splunkjs/mvc",
+    "jquery",
+    "splunkjs/mvc/simplesplunkview",
+    'text!../app/CHANGEME/js/templates/CHANGEMEView.html', // CHANGE_THIS: Modify the path to use a template
+    "css!../app/CHANGEME/css/CHANGEMEView.css" // CHANGE_THIS: Modify the path to use a template
+], function(
+    _,
+    Backbone,
+    mvc,
+    $,
+    SimpleSplunkView,
+    Template
+){
+    // Define the custom view class
+    var CHANGEMEView = SimpleSplunkView.extend({ // CHANGE_THIS
+        className: "CHANGEMEView", // CHANGE_THIS
+        
+        defaults: {
+        	
+        },
+        
+        initialize: function() {
+        	this.options = _.extend({}, this.defaults, this.options);
+        	
+        	//this.some_option = this.options.some_option;
+        },
+        
+        render: function () {
+        	
+        	this.$el.html(_.template(Template, {
+        		//'some_option' : some_option
+        	}));
+        	
+        }
+    });
+    
+    return CHANGEMEView; // CHANGE_THIS
+});
