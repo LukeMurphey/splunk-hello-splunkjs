ef75c6771fd8c862104e46f24de02e531075b968
Luke Murphey <Luke@LukeMurphey.net>
2016-04-21 21:36:34 -0500

    Adding the search functionality to display the most recent event

--- src/appserver/static/js/views/HelloSplunkJSView.js
+++ src/appserver/static/js/views/HelloSplunkJSView.js
@@ -41,8 +41,45 @@ define([
         	//this.some_option = this.options.some_option;
         },
         
+        
         doGetMostRecentEvent: function(){ 
         	
+        	// Make a search
+        	var search = new SearchManager({
+                "id": "get-most-recent-event-search",
+                "earliest_time": "-1h@h",
+                "latest_time": "now",
+                "search":'index=_internal OR index=main | head 1 | fields _raw',
+                "cancelOnUnload": true,
+                "autostart": false,
+                "auto_cancel": 90,
+                "preview": false
+            }, {tokens: true});
+            
+        	
+        	search.on('search:failed', function() {
+                alert("Search failed");
+            }.bind(this));
+            
+        	search.on("search:start", function() {
+                console.log("Search started");
+            }.bind(this));
+            
+        	search.on("search:done", function() {
+                console.log("Search completed");
+            }.bind(this));
+        	
+            // Get a reference to the search results
+            var searchResults = search.data("results");
+            
+            // Process the results of the search when they become available
+            searchResults.on("data", function() {
+            	$("#most-recent-event", this.$el).val(searchResults.data().rows[0][0]);
+            }.bind(this));
+            
+            // Start the search
+            search.startSearch();
+            
         },
         
         render: function () {
